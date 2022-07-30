1b87c87a8a14df205c865308cb553e00be8f264a
FreeRotate <lautrueyes@163.com>
2022-07-30 20:47:57 +0800

    fix

---
+++ train.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File  : train.py
+# @Author: LauTrueYes
+# @Date  : 2022/7/30 20:45
+
+def train(model, train_loader, dev_loader, config, vocab):
+    pass
