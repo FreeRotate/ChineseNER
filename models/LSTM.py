52bf26d938171924589121eee3a23b8cdcb4ecfa
FreeRotate <lautrueyes@163.com>
2022-08-01 13:27:10 +0800

    LSTM

--- models/LSTM.py
+++ models/LSTM.py
@@ -5,29 +5,36 @@
 # @Date  : 2022/8/1 9:11
 import torch
 import torch.nn as nn
+from torchcrf import CRF
 
 class Model(nn.Module):
     def __init__(self, vocab_len, config):
         super().__init__()
         self.num_labels = config.num_labels
         self.embed = nn.Embedding(num_embeddings=vocab_len, embedding_dim=config.embed_dim)
-        self.lstm = nn.LSTM(input_size=config.embed_dim, hidden_size=config.embed_dim, bidirectional=True)
-        self.fc = nn.Linear(config.embed_dim * 2, config.num_labels) #分类
         self.dropout = nn.Dropout(config.dropout_rate)
-        self.ln = nn.LayerNorm(config.embed_dim * 2)
+        self.lstm = nn.LSTM(input_size=config.embed_dim, hidden_size=config.embed_dim, batch_first=True, bidirectional=True)
+        self.layer_norm = nn.LayerNorm(config.embed_dim * 2)
+        self.classifier = nn.Linear(config.embed_dim * 2, config.num_labels)
+        self.crf = CRF(num_tags=config.num_labels, batch_first=True)
         self.loss_fct = nn.CrossEntropyLoss()
 
-    def forward(self, word_ids, label_ids=None, label_mask=None):
+    def forward(self, word_ids, label_ids=None, label_mask=None, use_crf=True):
         word_embed = self.embed(word_ids)
-        lstm_embed, _ = self.lstm(word_embed)
-        lstm_embed = self.dropout(lstm_embed)
-        lstm_embed = self.ln(lstm_embed)
-        label_predict = self.fc(lstm_embed)
+        word_embed = self.dropout(word_embed)
+        sequence_output, _ = self.lstm(word_embed)
+        sequence_output = self.layer_norm(sequence_output)
+        logits = self.classifier(sequence_output)
         if label_ids != None:
-            active_logits = label_predict.view(-1, self.num_labels)
-            active_labels = torch.where(label_mask.view(-1), label_ids.view(-1), self.loss_fct.ignore_index)
-            loss = self.loss_fct(active_logits, active_labels)
+            if use_crf:
+                loss = self.crf(logits, label_ids)
+                loss = -1 * loss
+            else:
+                active_logits = logits.view(-1, self.num_labels)
+                active_labels = torch.where(label_mask.view(-1), label_ids.view(-1), self.loss_fct.ignore_index)
+                loss = self.loss_fct(active_logits, active_labels)
+
         else:
             loss = None
 
-        return loss, label_predict.argmax(dim=-1)
+        return loss, logits.argmax(dim=-1)
